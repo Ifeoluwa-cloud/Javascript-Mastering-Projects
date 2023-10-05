e3a2284a2bbb40dce266c779383af4f50ad66a5f
Ifeoluwa Gbolahan <ifeoluwag90@gmail.com>
2023-10-05 15:00:14 +0100

    feat: Reviews Project Added

--- Counter/app.js
+++ Counter/app.js
@@ -4,3 +4,31 @@ let count = 0;
 // select value and buttons
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    //console.log(btn);
+    btn.addEventListener('click', function(e){
+        const myEvent = e.currentTarget.classList;
+        console.log(myEvent);
+        if (myEvent.contains('decrease')){
+            count--;
+        }
+        else if(myEvent.contains('increase')){
+            count++;
+        }
+        else{
+            count = 0;
+        }
+        value.textContent = count;
+
+        if(count > 0){
+            value.style.color = 'green';
+        }
+        else if(count < 0){
+            value.style.color = 'red';
+        }
+        else{
+            value.style.color = '#222';
+        }
+    })
+})
--- Random Background Generator/app.js
+++ Random Background Generator/app.js
@@ -6,16 +6,14 @@ const color = document.querySelector(".color");
 btn.addEventListener("click", function(){
     // get random number between 0 - 3;
     const randomNumber = getRandomNumber();
-    
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
 });
-
 function getRandomNumber() {
     return Math.floor(Math.random() * colors.length);
 }
 
-/**
+/** Or use Click to get background color sequentially instead of randomly;
     const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
     const btn = document.getElementById('btn');
     const color = document.querySelector(".color");
